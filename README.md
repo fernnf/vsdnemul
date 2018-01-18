86604d1bec5ae20d068db51ace4229c43e236444
Fernando Farias <fernnf@gmail.com>
2018-01-18 11:23:13 -0300

    Complete README

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# vsdnagent
-
+# vSDNAgent
+vSDNAgent  an SDN library for developing computer topology based on container docker. The library is able to create a computer, switches, router or another device for building your owner topology more realistic as possible. Furthermore, the developer can create device templates, with services or applications for working together on topology.
+  
--- setup.py
+++ setup.py
@@ -1,14 +1,26 @@
 from distutils.core import setup
 
-setup(
-    name = 'vsdnagent',
-    version = '0.1',
-    packages = ['vsdnagent'],
-    url = 'https://github.com/FernandoFarias/vsdnagent',
-    license = 'Apache2',
-    author = 'Fernando Farias',
-    author_email = 'fernnf@gmail.com',
-    description = 'A SDN prototyper for create network topology for rapid developer application.',
-    requires = ['pyroute2', 'docker', 'cmd2', 'requests', 'names']
-)
 
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(name = 'vsdnagent',
+      version = '0.1',
+      long_description = readme(),
+      packages = ['vsdnagent'],
+      url = 'https://github.com/FernandoFarias/vsdnagent',
+      classifiers = [
+          'Development Status :: 0.1 - Release',
+          'License :: OSI Approved :: APACHE2 License',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: SDN Develop :: Network Emulator',
+      ],
+      license = 'Apache2',
+      author = 'Fernando Farias',
+      author_email = 'fernnf@gmail.com',
+      description = 'A SDN topology emulator to create specific topologies for research and development in SDN',
+      install_requires = ['pyroute2', 'docker', 'cmd2', 'requests', 'names'],
+      include_package_data = True,
+      zip_safe = False)
