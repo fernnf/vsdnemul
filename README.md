e33821e5b7755a2332941cff39a195339abb926f
Fernando Farias <fernnf@gmail.com>
2018-01-18 19:30:52 -0300

    move to version 0.1

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # vSDNAgent
+
 vSDNAgent  an SDN library for developing computer topology based on container docker. The library is able to create a computer, switches, router or another device for building your owner topology more realistic as possible. Furthermore, the developer can create device templates, with services or applications for working together on topology.
+
+### Requirements 
+
+ - [Docker ](www.docker.com) (17.12.0-ce or later)
+ - [Python](www.python.org) (3.6 or later)
+ - [OpenvSwitch ](openvswitch.org) (2.7 or later)
+ - [IPRoute2](http://kernel.org/pub/linux/utils/net/iproute2/) (4.11.0 or later)
+ - Any Linux OS (4.3 or later) - (Developed and Tested on Fedora 26) 
+
+### Installation
+
+1. Clone vsdnagent from github: 
+> $ git clone https://github.com/FernandoFarias/vsdnagent.git
+
+2. Install the vsdnagent library:
+> $ cd vsdnagent 
+> 
+>  $ chmod +x install.sh
+>  
+> $ ./install.sh all 
+3. Test a simple topology script
+> $ cd examples
+>
+> $ sudo python3 simplotopo.py
   
---
+++ install.sh
@@ -0,0 +1,97 @@
+#!/usr/bin/env bash
+
+PY="/usr/bin/python3"
+PP="/usr/bin/pip3"
+DC="/usr/bin/docker"
+
+
+function check {
+    echo "Checking the need command for library ..."
+    CMD=($PY $DC $PP)
+    for i in "${CMD[@]}"
+    do
+        if [ -e "${i}" ]
+        then
+            echo "${i} ... OK"
+        else
+            echo "${i} ... FAILED"
+            echo "${i} command not found"
+            exit
+        fi
+    done
+    echo "DONE"
+}
+
+function template {
+
+    HOST=templates/host/Dockerfile
+    ONOS=templates/onos/Dockerfile
+    WHITEBOX=templates/whitebox/Dockerfile
+
+    echo "Installing templates devices ..."
+    echo "* HOST computer template"
+    $DC build --rm -f ${HOST} -t vsdn/host:latest --no-cache=true . &> /dev/null
+    if [ $? -ne 0 ]
+    then
+        echo "Cannot install host template in docker"
+        exit
+    fi
+    echo "* ONOS network controller template"
+    $DC build --rm -f ${ONOS} -t vsdn/onos:latest --no-cache=true . &> /dev/null
+    if [ $? -ne 0 ]
+    then
+        echo "Cannot install onos template in docker"
+        exit
+    fi
+    echo "* WHITEBOX network switch template"
+    $DC build --rm -f ${WHITEBOX} -t vsdn/whitebox:latest --no-cache=true . &> /dev/null
+    #&> /dev/null
+    if [ $? -ne 0 ]
+    then
+        echo "Cannot install whitebox template in docker"
+        exit
+    fi
+    echo "DONE"
+
+}
+
+function library {
+    SUDO="/usr/bin/sudo"
+    echo "Installing vSDNAgent python library ..."
+    ${SUDO} ${PP} install . &> /dev/null
+    if [ $? -ne 0 ]
+    then
+        echo "Cannot install library "
+        exit
+    fi
+    echo "DONE"
+}
+
+function all {
+    check
+    template
+    library
+}
+
+case $1 in
+    check)
+        check
+        ;;
+    template)
+        template
+        ;;
+    library)
+        library
+        ;;
+    all)
+        all
+        ;;
+    *)
+        echo "option not found"
+        echo "please use the following options:"
+        echo "  [check] --------- for checking command necessaries."
+        echo "  [template] ------ for installing devices templates."
+        echo "  [library] ------- for installing only library."
+        echo "  [all] ----------- for executing all options before"
+esac
+
