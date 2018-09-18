5b04a75949e761fed8451b98807ce47af918a690
Fernando Farias <fernnf@gmail.com>
2018-09-18 14:44:35 -0300

    Migrate to 0.2 version

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ def readme():
 
 
 setup(name = 'vsdnemul',
-      version = '0.1',
+      version = '0.2',
       long_description = readme(),
       packages = find_packages(),
       url = 'https://github.com/FernandoFarias/vsdnemul',
       classifiers = [
-          'Development Status :: 0.1 - Release',
+          'Development Status :: 0.2 - Release',
           'License :: OSI Approved :: APACHE2 License',
           'Programming Language :: Python :: 3.6',
           'Topic :: SDN Develop :: Network Emulator',
@@ -22,6 +22,6 @@ setup(name = 'vsdnemul',
       author_email = 'fernnf@gmail.com',
       description = 'A SDN topology emulator to create specific topologies for research and development in SDN',
       keywords = "SDN Networking Emulator",
-      install_requires = ['pyroute2', 'docker', 'cmd2', 'requests', 'names'],
+      install_requires = ['pyroute2', 'docker', 'cmd2', 'requests', 'names', 'ryu'],
       include_package_data = True,
       zip_safe = False)
